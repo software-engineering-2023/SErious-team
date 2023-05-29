bc4d843c1956f558dddaae54c549fe4bac6e7403
Mohamed Mohey <mmi9433@gmail.com>
2023-05-29 23:38:46 +0300

    edited bonus

--- milestone_3/addBonus.js
+++ milestone_3/addBonus.js
@@ -1,7 +1,7 @@
-document.getElementById("submit").addEventListener("click", function() {
+document.getElementById("submit-Bonus").addEventListener("click", function() {
     var name = document.getElementById("name").value;
     var id = document.getElementById("id").value;
-    var address = document.getElementById("Bonus Ammount").value;
+    var address = document.getElementById("Bonus-Ammount").value;
     if (name == "" || id == ""  || address == "" || Bonus-Ammount == "" ) {
         alert("Please fill in all fields");
     }
@@ -21,7 +21,7 @@ document.getElementById("submit").addEventListener("click", function() {
 })
 
 document.getElementById("back").addEventListener("click", function() {
-    window.location.href = "AdminDashBoard.html";
+    window.location.href = "BankerDashBoard.html";
 
     
 })
